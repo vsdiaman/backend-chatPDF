03b3ece09b92ae476ba219d0412f83a8139e6669
vsdiaman <vitor.diamantino8@gmail.com>
2025-02-04 00:07:54 -0300

    configurando pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'backend-nest',
+      script: 'dist/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
