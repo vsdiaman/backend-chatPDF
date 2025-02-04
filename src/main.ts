b766315947ffec7bf04abd52f9eed73c739643d6
vsdiaman <vitor.diamantino8@gmail.com>
2025-02-03 23:37:47 -0300

    reduzindo memoria

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT;
-  app.enableCors();
-  await app.listen('Porta:' + port);
+  const port = process.env.PORT || 4000; // Render define a porta automaticamente
+  await app.listen(port);
+  console.log(`🚀 Server is running on port ${port}`);
 }
 bootstrap();
-setInterval(() => {
-  const used = process.memoryUsage();
-  console.log(`Heap: ${Math.round(used.heapUsed / 1024 / 1024)} MB`);
-}, 10000); // A cada 10 segundos
